aaebf4e894915c1226734c248da4990f11eb4533
Mykhailo Pazyniuk <mishanya@protonmail.com>
2019-02-22 13:22:08 +0200

    Added example modules.
    2 example modules, showing workflow of an API.

---
+++ nasa_apod.py
@@ -0,0 +1,10 @@
+import json
+import urllib.request
+import pprint
+import webbrowser
+
+
+URL = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+jsonFILE = json.loads(urllib.request.urlopen(URL).read())
+#pprint.pprint(jsonFILE)
+webbrowser.open(jsonFILE['url'])
