2d46e85bdac23472b27db89e66a296f7bd791ed5
Mykhailo Pazyniuk <mishanya@protonmail.com>
2019-02-22 13:25:51 +0200

    Update and rename nasa_apod.py to examples/nasa_apod.py
    Moved files to another folder.

--- examples/nasa_apod.py
+++ examples/nasa_apod.py
@@ -6,5 +6,5 @@ import webbrowser
 
 URL = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
 jsonFILE = json.loads(urllib.request.urlopen(URL).read())
-#pprint.pprint(jsonFILE)
+pprint.pprint(jsonFILE)
 webbrowser.open(jsonFILE['url'])
