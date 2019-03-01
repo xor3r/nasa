a595472c5e88ba683fdb2b416c685f10bc88c5c7
Mykhailo Pazyniuk <mishanya@protonmail.com>
2019-03-01 21:30:14 +0200

    Update parse_location.py

--- examples/parse_location.py
+++ examples/parse_location.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 from xml.dom import minidom
 import urllib.request
 
@@ -14,24 +13,3 @@ featureCollection = list()
 print(lon[0].firstChild.nodeValue)
 print(lat[0].firstChild.nodeValue)
 print(len(lat), len(lon))
-
-
-=======
-from xml.dom import minidom
-import urllib.request
-
-
-locs = 'http://mars.jpl.nasa.gov/msl-raw-images/locations.xml'
-
-data = urllib.request.urlopen(locs).read()
-xmldoc = minidom.parseString(data)
-lon = xmldoc.getElementsByTagName('lon')
-lat = xmldoc.getElementsByTagName('lat')
-featureCollection = list()
-
-print(lon[0].firstChild.nodeValue)
-print(lat[0].firstChild.nodeValue)
-print(len(lat), len(lon))
-
-
->>>>>>> Stashed changes
