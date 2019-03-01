6e6575ac4263df9bfabb7587fb856e509579d007
Mykhailo Pazyniuk <mishanya@protonmail.com>
2019-03-01 21:28:08 +0200

    Update parse_by_sol.py

--- examples/parse_by_sol.py
+++ examples/parse_by_sol.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import json
 import urllib.request
 import pprint
@@ -15,21 +14,3 @@ link = jsonFILE['images'][0]['urlList']
 
 print('Camera type: {0}, Sol #{1}'.format(camera, sol))
 webbrowser.open(link)
-=======
-import json
-import urllib.request
-import pprint
-import webbrowser
-
-
-URL = "https://mars.jpl.nasa.gov/msl-raw-images/image/images_sol2320.json"
-jsonFILE = json.loads(urllib.request.urlopen(URL).read())
-#pprint.pprint(jsonFILE)
-
-camera = jsonFILE['images'][0]['cameraModelType']
-sol = jsonFILE['images'][0]['sol']
-link = jsonFILE['images'][0]['urlList']
-
-print('Camera type: {0}, Sol #{1}'.format(camera, sol))
-webbrowser.open(link)
->>>>>>> Stashed changes
