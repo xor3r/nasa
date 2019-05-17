efb9421968daf6dd6adfdc82fd4dadee0ee28878
Mykhailo Pazyniuk <mishanya@protonmail.com>
2019-05-17 11:06:51 +0300

    Modified setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='flaskr',
+    name='nasa',
     version='1.0.0',
+    description='Data receiver from Curiosity rover on Mars',
+    author='Mykhailo Pazyniuk',
+    author_email='mishanya@protonmail.com',
+    url='https://github.com/damoklov/nasa',
+    download_url='https://github.com/damoklov/nasa.git',
+    keywords=['nasa', 'python', 'Mars'],
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: Stable",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Web Applications :: NASA API",
+        ],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
